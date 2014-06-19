c5eb7d0477aa896a256145daa20beed36bdc5a2d
sonyone97 <himanshusonycs@gmail.com>
2014-06-19 12:14:22 +0530

    created open.java
    new

--- src/trainee/open.java
+++ src/trainee/open.java
@@ -1,10 +1,10 @@
 package trainee;
 
-public class main {
+public class open {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello commit.");
+		System.out.print("new open");
 	}
 
 }
